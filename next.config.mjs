77d3dcfcb1ab316993e116f5f642aba39d790e6e
Raymie Segars <raymievsegarsiv@gmail.com>
2024-05-01 19:28:10 -0400

    added first model for the website alongside the skybox
    integrating url-loader with next.js to load the .glb files for models we are able to now add models with three.js and react.three and display them in the browser

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.glb$/,
+      use: [
+        {
+          loader: 'url-loader',
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,47 @@
 "use client";
 import React from "react";
+import { Suspense } from "react";
 import Navbar from "./Navbar";
 import { Canvas } from "@react-three/fiber";
+import Loader from "./Loader";
+import Island from "@/models/Island";
+import Sky from "@/models/Sky";
 
-{/* <Navbar />
-<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center"></div> */}
+{
+  /* <Navbar />
+<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center"></div> */
+}
 
 const HomePage = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null 
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0]
+
+    if(window.innerWidth < 768) {
+      screenScale= [0.9, 0.9, 0.9];
+    } else {
+      screenScale= [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
-      <Canvas className="w-full h-screen bg-transparent">
-
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+          <Sky />
+          <Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
+        </Suspense>
       </Canvas>
     </section>
   );
