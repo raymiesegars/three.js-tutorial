0281674718ff94b31335b39f0c147d6df1bb8ae7
Raymie Segars <raymievsegarsiv@gmail.com>
2024-05-02 11:13:37 -0400

    added checking for the window existing to avoid build errors

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -61,10 +61,12 @@ const HomePage = () => {
     let screenPosition = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
 
-    if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
-    } else {
-      screenScale = [1, 1, 1];
+    if (typeof window !== "undefined") {
+      if (window.innerWidth < 768) {
+        screenScale = [0.9, 0.9, 0.9];
+      } else {
+        screenScale = [1, 1, 1];
+      }
     }
 
     return [screenScale, screenPosition, rotation];
@@ -74,12 +76,14 @@ const HomePage = () => {
     let screenScale, screenPosition;
     let rotation = [0.1, 4.7, 0];
 
-    if (window.innerWidth < 768) {
-      screenScale = [1.5, 1.5, 1.5];
-      screenPosition = [0, -1.5, 0];
-    } else {
-      screenScale = [3, 3, 3];
-      screenPosition = [0, -4, -4];
+    if (typeof window !== "undefined") {
+      if (window.innerWidth < 768) {
+        screenScale = [1.5, 1.5, 1.5];
+        screenPosition = [0, -1.5, 0];
+      } else {
+        screenScale = [3, 3, 3];
+        screenPosition = [0, -4, -4];
+      }
     }
 
     return [screenScale, screenPosition];
