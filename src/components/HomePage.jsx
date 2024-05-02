39e9ec4b650a44f038b5cbe405caeecb362ffc61
Raymie Segars <raymievsegarsiv@gmail.com>
2024-05-01 20:13:56 -0400

    added the ability to drag around the island model

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,13 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Suspense } from "react";
 import Navbar from "./Navbar";
 import { Canvas } from "@react-three/fiber";
 import Loader from "./Loader";
 import Island from "@/models/Island";
 import Sky from "@/models/Sky";
+import Bird from "@/models/Bird";
+import Plane from "@/models/Plane";
 
 {
   /* <Navbar />
@@ -13,34 +15,72 @@ import Sky from "@/models/Sky";
 }
 
 const HomePage = () => {
-  const adjustIslandForScreenSize = () => {
-    let screenScale = null 
-    let screenPosition = [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0]
+  const [isRotating, setIsRotating] = useState(false);
 
-    if(window.innerWidth < 768) {
-      screenScale= [0.9, 0.9, 0.9];
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
     } else {
-      screenScale= [1, 1, 1];
+      screenScale = [1, 1, 1];
     }
 
     return [screenScale, screenPosition, rotation];
-  }
+  };
 
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
+  const [planeScale, planePosition, planeRotation] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
+      {/* <Navbar /> */}
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent" ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
-          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          <Bird />
           <Sky />
-          <Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+          />
+          <Plane
+            planeScale={planeScale}
+            planePosition={planePosition}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
